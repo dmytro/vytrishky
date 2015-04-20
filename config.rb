b47e648144d5f21e4c857c193edd12ead8f38d6d
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2015-04-20 10:58:28 +0900

    config from startpack

--- config.rb
+++ config.rb
@@ -2,9 +2,7 @@
 require 'slim'
 Slim::Engine.disable_option_validator!
 
-#$. << "./lib"
-require "#{File.dirname(__FILE__)}/lib/products"
-require "#{File.dirname(__FILE__)}/lib/data_folders"
+require 'pry'
 
 I18n.enforce_available_locales = true
 # general settings
@@ -14,146 +12,116 @@ set :css_dir, 'assets/stylesheets'
 set :js_dir, 'assets/javascripts'
 set :images_dir, 'assets/images'
 set :partials_dir, 'partials'
-set :haml, { :format => :html5 }
-
-ignore '*.less'
-
-# don't precompile assets when middleman startup, but only when it's requested.
-#set :debug_assets, true
 
 activate :livereload
 
-activate :i18n, langs: [:ja, :en, :uk]
-
 activate :directory_indexes
 
-page '/', layout: 'layout'
+activate :blog
 
-set :markdown, :tables => true, :autolink => true, :gh_blockcode => true, :fenced_code_blocks => true, :with_toc_data => true
-set :markdown_engine, :redcarpet
+helpers do
+
+  def post_top_image
+    image_path data.page.top_image
+  end
+
+  def chapters( post )
+    headers = File.readlines( post.source_file ).collect do |x|
+      if x =~ /^\#{1,6}\s(.*)/
+        $1
+      else
+        nil
+      end
+    end.compact
+
+
+    case markdown_engine
+    when :redcarpet
+      headers.map { |x| [x, x.downcase.gsub( /\s/, "-" )] }
+    when :kramdown
+      headers.each_with_index.map { |x,i| [x,i == 0 ? "section" : "section-#{i}"] }
+    else
+      []
+    end
+  end
+
+  def image_caption(image, text)
+    %{<figure>
+      #{image_tag image}
+      <figcaption> #{text} </figcaption>
+   </figure>}
+  end
+
+  def page_active?(urls)
+    Array(urls).each do |url|
+      return true if current_page.url.split("/").last == url.split("/").last
+    end
+    false
+  end
+
+  def active_class(url)
+    "active" if page_active?(url)
+  end
+end
+
+# set :markdown, tables: true, autolink: true, gh_blockcode: true, fenced_code_blocks: true, with_toc_data: true, smart: true
+# set :markdown_engine, :redcarpet
+
+set :markdown_engine, :kramdown
+set :markdown, :layout_engine => :erb,
+  :tables => true,
+  :autolink => true,
+  footnotes: true,
+  :smartypants => true,
+  smart_quotes: [180, 180, 8222, 8220]
 
 configure :development do
+  set :base, ""
   activate :relative_assets
 end
 
-# --------------------------------------------
-# Localization helpers
-# --------------------------------------------
-helpers do
-
-  # Print content tag only if text present
-  def tag_if(text, tag, alternative_text=nil)
-    if text.blank?
-      content_tag(tag, alternative_text) if alternative_text
-    else
-      content_tag(tag, text)
-    end
-
-  end
-
-  def current_language
-    data.languages[I18n.locale]
-  end
-
-  def available_locales_as_regex
-    I18n.config.available_locales.map(&:to_s).join("|")
-  end
-
-  def current_without_locale
-    current_page.url
-    .sub(%r{^/(#{ available_locales_as_regex})/},'/')
-  end
-
-  def current_with_locale(locale)
-    if locale == I18n.default_locale.to_s
-      current_without_locale
-    else
-      "/#{locale}/#{current_without_locale}"
-    end
-      .gsub(%r{/+}, "/")
-  end
-
-  # Translate strings that are not part of /locale/ directory.
-  def l(key, split: false)
-    string = ((key.is_a?(Hash) ? key[I18n.locale.to_s] : key) || "")
-    if split
-      string.gsub(/\n+/, "<p>")
-    else
-      string
-    end
-  end
-
-  def locale_prefix(locale=I18n.locale)
-    if locale == I18n.default_locale
-      "/"
-    else
-      "/#{locale.to_s}"
-    end
-  end
-
-  def localized_href(href)
-    "/#{locale_prefix}/#{href}".gsub(%r{/+}, "/")
-  end
-
-end
-
-helpers do
-  def products_item_link(item)
-    "<a href='#{localized_href item.url}'>#{item.basename.to_i}</a>"
-  end
-end
-
-activate :products
-activate :data_folders, namespace: 'events'
-
-# --------------------------------------------
-# Generated dynamic pages
-# --------------------------------------------
-[ :ja, :en, :uk].each do |lang|
-  pref = (lang == :ja) ? "" : "/#{lang}"
-
-  # --------------------------------------------
-  # DataFolders - Events
-  # --------------------------------------------
-  events.values.each do |event|
-    proxy "#{pref}/event/#{event.index}.html", "event.html", locals: {event: event}, ignore: true do
-      ::I18n.locale = lang
-    end
-  end
-
-  # --------------------------------------------
-  # Products is Pysanka products lister
-  # --------------------------------------------
-  Products::Items.list.each do |dir|
-    item = ::Products::Item.new(dir)
-    proxy "#{pref}/#{item.url}.html", "product.html", locals: { item: item}, ignore: true do
-      ::I18n.locale = lang
-    end
-  end
-
-end
-# --------------------------------------------
-
 configure :build do
+  set :base, "/vytrishky"
   activate :relative_assets
   # activate :directory_indexes
   activate :sprockets
   activate :minify_css
   activate :minify_javascript
+  set :relative_links, true
+
+  activate :disqus do |d|
+    d.shortname = 'vytrishky'
+  end
+
   # somehow minifying html takes some html attributes away so it is causing
   # some css not applied to certain elements... so until we find alternative
   # way to monify html, we will disable this
-  #activate :minify_html
+  activate :minify_html do |html|
+    html.remove_multi_spaces        = true   # Remove multiple spaces
+    html.remove_comments            = true   # Remove comments
+    html.remove_intertag_spaces     = false  # Remove inter-tag spaces
+    html.remove_quotes              = true   # Remove quotes
+    html.simple_doctype             = false  # Use simple doctype
+    html.remove_script_attributes   = false   # Remove script attributes
+    html.remove_style_attributes    = false   # Remove style attributes
+    html.remove_link_attributes     = false   # Remove link attributes
+    html.remove_form_attributes     = false  # Remove form attributes
+    html.remove_input_attributes    = false   # Remove input attributes
+    html.remove_javascript_protocol = false   # Remove JS protocol
+    html.remove_http_protocol       = false   # Remove HTTP protocol
+    html.remove_https_protocol      = false  # Remove HTTPS protocol
+    html.preserve_line_breaks       = false  # Preserve line breaks
+    html.simple_boolean_attributes  = true   # Use simple boolean attributes
+  end
   activate :asset_hash
-  #activate :gzip
-  ignore 'product.html'
+  activate :gzip
+  ignore '*.less'
   ignore(/Icon\r$/)
   ignore(/\.DS_Store/)
   ignore(/^assets.*\.yml/)
   ignore(/^assets\/stylesheets\/(?!all).*\.css/)
   ignore(/^assets\/javascripts\/(?!all).*\.js/)
   ignore(%r{^assets/stylesheets/colorschemas/.*})
-  ignore(%r{^assets/images/homepage-slider/pysanka-.*\.jpg$})
   ignore(%r{/src/})
 
 
