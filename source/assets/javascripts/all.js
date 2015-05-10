148e6f63c4105d76f0c5e0b63ddadb4f1fce06bc
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2015-05-10 13:00:25 +0900

    Fancybox gallery, all images on page.

--- source/assets/javascripts/all.js
+++ source/assets/javascripts/all.js
@@ -1,30 +1,32 @@
 /*
 
-*= require jquery.min
-*= require bootstrap.min
-*= require jquery.fancybox
-*= require clean-blog.min
+ *= require jquery.min
+ *= require bootstrap.min
+ *= require jquery.fancybox
+ *= require clean-blog.min
 
-*/
+ */
 
 $(document).ready(function() {
     $(document).ready(function() {
-		$(".fancybox").fancybox({
-            padding    : 0,
-            margin     : 10,
-            nextEffect : 'fade',
-            prevEffect : 'none',
-            autoCenter : false,
-            afterLoad  : function () {
-                $.extend(this, {
-                    aspectRatio : false,
-                    type    : 'html',
-                    width   : '100%',
-                    height  : '100%',
-                    content : '<div class="fancybox-image" style="background-image:url(' + this.href + '); background-size: cover; background-position:50% 50%;background-repeat:no-repeat;height:100%;width:100%;" /></div>'
-            });
-            }
-        })
+		$(".fancybox")
+            .attr('rel', 'gallery')
+            .fancybox({
+                padding    : 5,
+                margin     : 0,
+                nextEffect : 'fade',
+                prevEffect : 'none',
+                autoCenter : false,
+                afterLoad  : function () {
+                    $.extend(this, {
+                        aspectRatio : false,
+                        type    : 'html',
+                        width   : '97%',
+                        height  : '97%',
+                        content : '<div class="fancybox-image" style="background-image:url(' + this.href + '); background-size: contain; background-position:50% 50%;background-repeat:no-repeat;height:100%;width:100%;" /></div>'
+                    });
+                }
+            })
 	});
 
 });
