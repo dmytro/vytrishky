89eac6c73d3a661558043816942e1d2e6130e7ca
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2015-05-13 19:08:45 +0900

    Blog config.

--- config.rb
+++ config.rb
@@ -17,7 +17,31 @@ activate :livereload
 
 activate :directory_indexes
 
-activate :blog
+activate :blog do |blog|
+  # This will add a prefix to all links, template references and source paths
+  # blog.prefix = "blog"
+
+  # blog.permalink = "{year}/{month}/{day}/{title}.html"
+  # Matcher for blog source files
+  # blog.sources = "{year}-{month}-{day}-{title}.html"
+  # blog.taglink = "tags/{tag}.html"
+  # blog.layout = "layout"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  # blog.year_link = "{year}.html"
+  # blog.month_link = "{year}/{month}.html"
+  # blog.day_link = "{year}/{month}/{day}.html"
+  # blog.default_extension = ".markdown"
+
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
+
+  # Enable pagination
+  # blog.paginate = true
+  # blog.per_page = 10
+  # blog.page_link = "page/{num}"
+end
+
 
 helpers do
 
@@ -86,13 +110,19 @@ helpers do
   def active_class(url)
     "active" if page_active?(url)
   end
+
+  def markdown(&block)
+    raise ArgumentError, "Missing block" unless block_given?
+    content = capture_html(&block)
+    concat Tilt['markdown'].new { content }.render
+  end
 end
 
 # set :markdown, tables: true, autolink: true, gh_blockcode: true, fenced_code_blocks: true, with_toc_data: true, smart: true
 # set :markdown_engine, :redcarpet
 
 set :markdown_engine, :kramdown
-set :markdown, :layout_engine => :erb,
+set :markdown, :layout_engine => :slim,
   :tables => true,
   :autolink => true,
   footnotes: true,
