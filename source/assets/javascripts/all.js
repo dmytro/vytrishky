5fe62bc834bd3d29303b01238f8c42695e335c09
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2015-05-14 12:19:07 +0900

    remove extra level of calls

--- source/assets/javascripts/all.js
+++ source/assets/javascripts/all.js
@@ -8,25 +8,22 @@
  */
 
 $(document).ready(function() {
-    $(document).ready(function() {
-		$(".fancybox")
-            .attr('rel', 'gallery')
-            .fancybox({
-                padding    : 5,
-                margin     : 0,
-                nextEffect : 'fade',
-                prevEffect : 'none',
-                autoCenter : false,
-                afterLoad  : function () {
-                    $.extend(this, {
-                        aspectRatio : false,
-                        type    : 'html',
-                        width   : '97%',
-                        height  : '97%',
-                        content : '<div class="fancybox-image" style="background-image:url(' + this.href + '); background-size: contain; background-position:50% 50%;background-repeat:no-repeat;height:100%;width:100%;" /></div>'
-                    });
-                }
-            })
-	});
-
+	$(".fancybox")
+        .attr('rel', 'gallery')
+        .fancybox({
+            padding    : 5,
+            margin     : 0,
+            nextEffect : 'fade',
+            prevEffect : 'none',
+            autoCenter : false,
+            afterLoad  : function () {
+                $.extend(this, {
+                    aspectRatio : false,
+                    type    : 'html',
+                    width   : '97%',
+                    height  : '97%',
+                    content : '<div class="fancybox-image" style="background-image:url(' + this.href + '); background-size: contain; background-position:50% 50%;background-repeat:no-repeat;height:100%;width:100%;" /></div>'
+                });
+            }
+	})
 });
