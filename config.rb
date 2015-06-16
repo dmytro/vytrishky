27b531e3bb26bff5048cb6cba50fea8555514fd9
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2015-06-16 17:27:08 +0900

    Custom background per page (for tags,years).

--- config.rb
+++ config.rb
@@ -50,6 +50,16 @@ end
 
 helpers do
 
+  def custom_intro_background
+    image = "#{current_page.path.sub(/\.html$/,'')}.jpg"
+    prefix = "assets/images/backgrounds/"
+
+    if file = sitemap.resources.find { |x| x.path =~ %r{#{prefix}#{image}}}
+      file.path
+    else
+      data.page.intro.image
+    end
+  end
   #
   # Layout helpers
   #
